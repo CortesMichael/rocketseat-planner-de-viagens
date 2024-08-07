42f27e0a3df36f1ff2c19067b2552aaa605d370b
Michael Cortes <160887118+CortesMichael@users.noreply.github.com>
2024-08-07 18:47:59 -0300

    Add files via upload

---
+++ planner/src/main/java/com/rocktseat/planner/activitiy/ActivityRequestPayload.java
@@ -0,0 +1,4 @@
+package com.rocktseat.planner.activitiy;
+
+public record ActivityRequestPayload(String title, String occurs_at) {
+}
---
+++ planner/src/main/java/com/rocktseat/planner/activitiy/ActivityResponse.java
@@ -0,0 +1,6 @@
+package com.rocktseat.planner.activitiy;
+
+import java.util.UUID;
+
+public record ActivityResponse(UUID activityId) {
+}
